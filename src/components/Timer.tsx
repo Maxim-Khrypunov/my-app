2211b9e26b616456a424df3d088d7928c27764a1
Maxim Khrypunov <khrypunovmax@gmail>
2023-03-04 19:20:32 +0200

    HW#_31

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -7,15 +7,12 @@ export const Timer:React.FC=()=>
     let stylesH2: React.CSSProperties= {fontSize:"2em",color: "red",textAlign:"center"}
     let stylesH3: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
     setTimeout(tic, 1000);
-   
     const [time, setTime] = React.useState(new Date());
     function tic()
     {
        setTime(new Date())
     } 
-
     setTimeout(switfColor,5000)
-  
     let [changer,newColor] = React.useState(stylesH3);
     function switfColor()
     {
