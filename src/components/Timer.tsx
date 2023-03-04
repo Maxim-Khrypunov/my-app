d33580b84e377bc877b0b925c6f32d52b1f72782
Maxim Khrypunov <khrypunovmax@gmail>
2023-03-04 19:18:32 +0200

    HW31

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,17 +1,34 @@
 import React from "react"
 export const Timer:React.FC=()=>
 {
+   
     const styles: React.CSSProperties= {backgroundColor:"lightblue", fontSize:"2em", color: "red",
     textAlign:"center"}
-    const stylesH2: React.CSSProperties= {fontSize:"2em", color: "red",textAlign:"center"}
+    let stylesH2: React.CSSProperties= {fontSize:"2em",color: "red",textAlign:"center"}
+    let stylesH3: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
     setTimeout(tic, 1000);
+   
     const [time, setTime] = React.useState(new Date());
     function tic()
     {
        setTime(new Date())
+    } 
+
+    setTimeout(switfColor,5000)
+  
+    let [changer,newColor] = React.useState(stylesH3);
+    function switfColor()
+    {
+        newColor(stylesH2); 
+        setTimeout(restart,5000);  
     }
+    function restart()
+    {
+       window.location.reload()
+    }
+    
     return <div>
-        <h2 style ={stylesH2}>Current time</h2>
-        <p style={styles}>{time.toLocaleTimeString()}</p>
+    <h2 className="text" style ={changer}> Current time </h2>
+    <p style={styles}>{time.toLocaleTimeString()}</p>
     </div>
-}
+    }
