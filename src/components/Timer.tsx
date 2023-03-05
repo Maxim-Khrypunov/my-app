5f9b6211044379918406596a1f4617b0789b9612
Maxim Khrypunov <khrypunovmax@gmail>
2023-03-05 11:35:42 +0200

    HW-31Finish

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -5,27 +5,39 @@ export const Timer:React.FC=()=>
     const styles: React.CSSProperties= {backgroundColor:"lightblue", fontSize:"2em", color: "red",
     textAlign:"center"}
     let stylesH2: React.CSSProperties= {fontSize:"2em",color: "red",textAlign:"center"}
-    let stylesH3: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
+    let stylesH2New: React.CSSProperties= {fontSize:"2em",color: "blue",textAlign:"center"}
     setTimeout(tic, 1000);
     const [time, setTime] = React.useState(new Date());
     function tic()
     {
        setTime(new Date())
     } 
-    setTimeout(switfColor,5000)
-    let [changer,newColor] = React.useState(stylesH3);
-    function switfColor()
-    {
-        newColor(stylesH2); 
-        setTimeout(restart,5000);  
-    }
-    function restart()
-    {
-       window.location.reload()
-    }
+    const intervalDuration = 10000;
+
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        setChanger((styleColor) => styleColor === stylesH2 ? stylesH2New : stylesH2);
+      }, intervalDuration);
+      return () => clearInterval(interval);
+    }, []);
+      
+    let [changer,setChanger] = React.useState(stylesH2);
+   //  setTimeout(switfColor,5000)
+   //  let [changer,newColor] = React.useState(stylesH2);
+   //  function switfColor()
+   //  {
+   //      newColor(stylesH2New); 
+   //      setTimeout(restart,5000);  
+   //  }
+   //  function restart()
+   //  {
+   //     window.location.reload()
+   //  }
     
     return <div>
     <h2 className="text" style ={changer}> Current time </h2>
     <p style={styles}>{time.toLocaleTimeString()}</p>
     </div>
     }
+
+
