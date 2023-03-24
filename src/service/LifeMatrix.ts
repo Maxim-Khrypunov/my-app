d9ea82fc7260ae092321cf15b4edc77ea03397fa
Maxim Khrypunov <khrypunovmax@gmail>
2023-03-24 09:50:51 +0200

    HW36

--- src/service/LifeMatrix.ts
+++ src/service/LifeMatrix.ts
@@ -1,14 +1,40 @@
-import { getRandomMatrix } from "../util/random"
 
 export default class LifeMatrix
 {
-    constructor(private _number:number[][]){}
+    constructor(private _numbers:number[][]){}
     get number(){
-        return this._number
+        return this._numbers
     }
-    nextStep():number[][]
-    {
-        this._number = getRandomMatrix(this._number.length, this._number.length, 0, 1)
-        return  this._number;
+    
+    nextStep(): number[][] {
+        this._numbers = this._numbers.map((__, i) => this.getNewRow(i));// дай ряд
+        return this._numbers;
     }
+    getNewRow(i: number): number[] {
+        return this._numbers[i].map((__, j) => this.getNewCell(i, j));
+    }
+    
+    getNewCell(i: number, j: number): number {
+        const matrixPart: number[][] = this.getMatrixPart(i, j);
+        const sumCells = matrixPart.reduce((res, row) => res + 
+        row.reduce((rSum, cell) => rSum + cell), 0);
+        return this._numbers[i][j] ? isLiveFromLive(sumCells) : isLiveFromDead(sumCells);
+    }
+    getMatrixPart(i: number, j: number): number[][] {
+        const sliceIndex1 = j == 0 ? j : j - 1;
+        const sliceIndex2 = j == this._numbers[0].length - 1 ? j + 1 : j + 2;
+        const savedNum = this._numbers[i][j];
+        this._numbers[i][j] = 0;
+        const res = [i - 1, i, i + 1].map(rowIndex => this._numbers[rowIndex] ? 
+            this._numbers[rowIndex].slice(sliceIndex1, sliceIndex2) : [0]);
+       this._numbers[i][j] = savedNum;
+        return res;
+    }
+}
+
+function isLiveFromLive(sumCells: number): number {
+    return +(sumCells == 2 || sumCells == 3);
+}
+function isLiveFromDead(sumCells: number): number {
+    return +(sumCells == 3);
 }
