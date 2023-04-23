69998db3fb69ebfaabe977813cc2d7162137d603
Maxim Khrypunov <khrypunovmax@gmail>
2023-04-23 18:28:35 +0300

    HW-43-styles

--- src/components/pages/ProductsClient.tsx
+++ src/components/pages/ProductsClient.tsx
@@ -11,11 +11,12 @@ export const ProductsClient: React.FC = () => {
     const products: ProductType[] = useSelector<any, ProductType[]>(state => state.productsState.products);
 
 
-    return <Box sx={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    return <Box sx={{backgroundImage: 'url(https://img4.goodfon.ru/wallpaper/nbig/5/fc/fon-vypechka-bulochki.jpg)',
+    width: "100%", height: "100%", display: "flex", justifyContent: "center",alignItems: "center",}}>
     <Grid container spacing={2} sx={{ marginTop: "20px", marginLeft: "5vw", marginRight: "5vw" }}>
         {products.map((product) => (
-            <Grid item xs={12} sm={6} md={4} lg={4} key={product.id}>
-                <Card sx={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+            <Grid item xs={12} sm={6} md={4} lg={4} key={product.id} >
+                <Card sx={{ backgroundColor:"beige",height: "100%", display: "flex", flexDirection: "column", justifyContent: "center" }}>
                     <CardMedia image={`image/${product.image}`}/>
                     <CardContent title={product.title} unit={product.unit} cost={product.cost} category={product.category}></CardContent>
                     <CardActions></CardActions>
